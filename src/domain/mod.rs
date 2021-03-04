f88034b985ba3e40665900436a939017cc272889
theredfish <did.julian@gmail.com>
2021-03-04 14:50:33 +0100

    Fix doc

--- src/domain/mod.rs
+++ src/domain/mod.rs
@@ -6,7 +6,7 @@
 //!
 //! BDD tests can take advantage of this domain layer and share the same data
 //! structures. In our case we define here the different structures that we need
-//! in our tests and their serde::Deserialize implementation.
+//! in our tests and their serde::{Deserialize, Serialize} implementation.
 
 pub mod asset;
 pub mod order;
@@ -16,7 +16,7 @@ pub mod time;
 #[derive(Deserialize, Serialize, Debug)]
 pub struct ApiResult<T> {
     /// array of error messages in the format of:
-    /// * <char-severity code><string-error category>:<string-error type>[:<string-extra info>]
+    /// * `<char-severity code><string-error category>:<string-error type>[:<string-extra info>]`
     /// * severity code can be E for error or W for warning
     pub error: Vec<String>,
     /// result of API call (may not be present if errors occur)
